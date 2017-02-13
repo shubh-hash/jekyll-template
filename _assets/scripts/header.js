f14f2b37387007f5d731f524dd9b204a195bed9b
shubh-hash <shubham.aggarwal@hashedin.com>
2017-02-13 19:13:11 +0530

    Hamburger working properly

--- _assets/scripts/header.js
+++ _assets/scripts/header.js
@@ -1,4 +1,9 @@
-function menuFunction() {
+function menuOpen() {
   $(".menu-options").css("display", "flex");
   $("hamburger-icon").css("display", "none");
+}
+
+function menuClose() {
+  $(".menu-options").css("display", "none");
+  $("hamburger-icon").css("display", "block");
 }
