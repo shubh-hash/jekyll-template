0e6eea36586a41b177386cde12546109acd6cc12
shubh-hash <shubham.aggarwal@hashedin.com>
2017-02-13 18:44:12 +0530

    Improved hamburger menu

---
+++ _assets/scripts/header.js
@@ -0,0 +1,7 @@
+function menuFunction() {
+  console.log("hjbjbjh");
+  $(".menu-options").css("display", "flex");
+  $("img").css("display", "none");
+  // $('menu-options').toggleClass('menu-options');
+  // document.getElementsByClassName("menu-options").classList.toggle('show');
+}
