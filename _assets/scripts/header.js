57d4292bbf98c8c89ecc8cabc3f593620a4d5181
shubh-hash <shubham.aggarwal@hashedin.com>
2017-02-13 19:04:52 +0530

    Identation and hamburger fixes

--- _assets/scripts/header.js
+++ _assets/scripts/header.js
@@ -1,7 +1,4 @@
 function menuFunction() {
-  console.log("hjbjbjh");
   $(".menu-options").css("display", "flex");
-  $("img").css("display", "none");
-  // $('menu-options').toggleClass('menu-options');
-  // document.getElementsByClassName("menu-options").classList.toggle('show');
+  $("hamburger-icon").css("display", "none");
 }
